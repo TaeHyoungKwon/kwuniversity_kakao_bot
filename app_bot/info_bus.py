fae4ccceda8f4582ef0e190d995ee8bed2830a85
TaeHyoungKwon <kth5604@gmail.com>
2016-12-10 07:16:22 +0900

    info_bus indent error

--- app_bot/info_bus.py
+++ app_bot/info_bus.py
@@ -100,17 +100,17 @@ def info_bus(busstop):
             msg_bus7 ="* {}\n{}\n{}\n\n".format(inf_dict2['bus_4'],inf_dict2['arrmsgsec1_4'],inf_dict2['arrmsgsec2_4'])   
 
             message = msg_top + msg_bus1 +  msg_bus2 +  msg_bus3  + msg_middle + msg_bus4 +  msg_bus5 +  msg_bus6 +  msg_bus7
-
-	 elif busstop == "월계삼거리":
-		msg_top =  "실시간 버스 도착정보 입니다.\n\n\n월계삼거리 -> 월계 방향 or 광운대역 방향\n\n"
-		msg_bus1 ="* {}\n{}\n{}\n\n".format(inf_dict1['bus_1'], inf_dict1['arrmsgsec1_1'], inf_dict1['arrmsgsec2_1'])
-		msg_bus2 ="* {}\n{}\n{}\n\n\n".format(inf_dict1['bus_2'], inf_dict1['arrmsgsec1_2'], inf_dict1['arrmsgsec2_2'])
-
-		msg_middle =  "월계삼거리(에뽕버거 앞) -> 광운대 정문 방향  \n\n"
-		msg_bus4 ="* {}\n{}\n{}\n\n".format(inf_dict2['bus_1'],inf_dict2['arrmsgsec1_1'],inf_dict2['arrmsgsec2_1'])
-		msg_bus5 ="* {}\n{}\n{}\n\n".format(inf_dict2['bus_2'],inf_dict2['arrmsgsec1_2'],inf_dict2['arrmsgsec2_2'])
-
-        message = msg_top + msg_bus1 +  msg_bus2 + msg_middle + msg_bus4 +  msg_bus5
+        
+        elif busstop == "월계삼거리":
+            msg_top =  "실시간 버스 도착정보 입니다.\n\n\n월계삼거리 -> 월계 방향 or 광운대역 방향\n\n"
+            msg_bus1 ="* {}\n{}\n{}\n\n".format(inf_dict1['bus_1'], inf_dict1['arrmsgsec1_1'], inf_dict1['arrmsgsec2_1'])
+            msg_bus2 ="* {}\n{}\n{}\n\n\n".format(inf_dict1['bus_2'], inf_dict1['arrmsgsec1_2'], inf_dict1['arrmsgsec2_2'])
+            
+            msg_middle =  "월계삼거리(에뽕버거 앞) -> 광운대 정문 방향  \n\n"
+            msg_bus4 ="* {}\n{}\n{}\n\n".format(inf_dict2['bus_1'],inf_dict2['arrmsgsec1_1'],inf_dict2['arrmsgsec2_1'])
+            
+            msg_bus5 ="* {}\n{}\n{}\n\n".format(inf_dict2['bus_2'],inf_dict2['arrmsgsec1_2'],inf_dict2['arrmsgsec2_2'])
+            message = msg_top + msg_bus1 +  msg_bus2 + msg_middle + msg_bus4 +  msg_bus5
 
         result = {"text":message}
         end = time.time() - start
