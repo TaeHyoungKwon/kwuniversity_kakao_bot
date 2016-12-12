7d53f2359ac33e5c074764eb1a26b0a9d0c4d764
TaeHyoungKwon <kth5604@gmail.com>
2016-12-13 06:53:52 +0900

    subway change xml to json test

--- app_bot/info_subway.py
+++ app_bot/info_subway.py
@@ -3,99 +3,99 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-import requests_cache
-
 
 def info_subway():
-    requests_cache.install_cache('subway',expire_after=30)
 
-    start = time.time()
+    api = 'http://swopenAPI.seoul.go.kr/api/subway/435156415a6b74683131396b51665744/json/realtimeStationArrival/0/4/광운대' 
+
+    response = requests.get(api)
+    inf = BeautifulSoup(response.text, 'html.parser')
+    inf=json.loads(str(inf))
     
-    api = 'http://swopenAPI.seoul.go.kr/api/subway/435156415a6b74683131396b51665744/xml/realtimeStationArrival/0/5/광운대' 
-    try: 
+    if inf['errorMessage']['message'] == "해당하는 데이터가 없습니다.":
+        message = "현재는 운행 시간이 아닙니다.\n"
+        result = {"text":message}
+        return result
 
-        response = requests.get(api)
-        inf = BeautifulSoup(response.text, 'html.parser')
+    else:
 
+        inf_dict_down1 = {}
+        inf_dict_down2 = {}
+        inf_dict_up1 = {}
+        inf_dict_up2 = {}
 
-        if inf.find("message").text == "해당하는 데이터가 없습니다.":
-            message = "현재는 운행 시간이 아닙니다."
-            result = {"text":message}
-            return result
+        up = False
+        down = False
+        
+        
+        for a in inf['realtimeArrivalList']:
 
-        else:
+            if a['subwayId'] == '1001' and a['updnLine'] =="상행" and up == False:
+                inf_dict_up1["st_name"] = a['statnNm']
+                inf_dict_up1["st_updnline"] = a['updnLine']
+                inf_dict_up1["st_trainlinenm"] = a['trainLineNm'].split("-")[0]
+                inf_dict_up1["st_recptndt"] = a['recptnDt']
+                inf_dict_up1["st_arvlmsg2"] = a['arvlMsg2']
 
-            inf_dict_down1 = {}
-            inf_dict_down2 = {}
-            inf_dict_up1 = {}
-            inf_dict_up2 = {}
+                up = True
 
-            up = False
-            down = False
-
-            for a in inf.find_all("row"):
-
-                if a.find("subwayid").text == '1001' and a.find("updnline").text=="상행" and up == False:
-                    inf_dict_up1["st_name"] = a.find("statnnm").text
-                    inf_dict_up1["st_updnline"] = a.find("updnline").text
-                    inf_dict_up1["st_trainlinenm"] = a.find("trainlinenm").text.split("-")[0]
-                    inf_dict_up1["st_recptndt"] = a.find("recptndt").text
-                    inf_dict_up1["st_arvlmsg2"] = a.find("arvlmsg2").text
-
-                    up = True
-
-                if a.find("subwayid").text == '1001' and a.find("updnline").text=="상행" and up == True:
-                   
-                    inf_dict_up2["st_updnline"] = a.find("updnline").text
-                    inf_dict_up2["st_trainlinenm"] = a.find("trainlinenm").text.split("-")[0]
-                    inf_dict_up2["st_arvlmsg2"] = a.find("arvlmsg2").text
-
-
-                if a.find("subwayid").text == '1001' and a.find("updnline").text=="하행" and down == False:
-                    inf_dict_down1["st_recptndt"] = a.find("recptndt").text
-                    inf_dict_down1["st_updnline"] = a.find("updnline").text
-                    inf_dict_down1["st_trainlinenm"] = a.find("trainlinenm").text.split("-")[0]
-                    inf_dict_down1["st_arvlmsg2"] = a.find("arvlmsg2").text
-                    down = True
-
-
-                if a.find("subwayid").text == '1001' and a.find("updnline").text=="하행" and down == True:
-                    inf_dict_down2["st_trainlinenm"] = a.find("trainlinenm").text.split("-")[0]
-                    inf_dict_down2["st_arvlmsg2"] = a.find("arvlmsg2").text
+            if a['subwayId'] == '1001' and a['updnLine'] =="상행" and up == True:
+                inf_dict_up2["st_name"] = a['statnNm']
+                inf_dict_up2["st_updnline"] = a['updnLine']
+                inf_dict_up2["st_trainlinenm"] = a['trainLineNm'].split("-")[0]
+                inf_dict_up2["st_recptndt"] = a['recptnDt']
+                inf_dict_up2["st_arvlmsg2"] = a['arvlMsg2']
 
 
 
-            msg_intro = "광운대역 지하철 실시간 도착정보 입니다.\n\n"
-            msg_time = "기준 시각 : {}\n\n".format(inf_dict_up1['st_recptndt'])
-            msg_info_up = ("# {}\n{} - {}\n{} - {}\n\n".format(
+            if a['subwayId'] == '1001' and a['updnLine'] =="하행" and down == False:
+                inf_dict_down1["st_name"] = a['statnNm']
+                inf_dict_down1["st_updnline"] = a['updnLine']
+                inf_dict_down1["st_trainlinenm"] = a['trainLineNm'].split("-")[0]
+                inf_dict_down1["st_recptndt"] = a['recptnDt']
+                inf_dict_down1["st_arvlmsg2"] = a['arvlMsg2']
 
-                    inf_dict_up1['st_updnline'], 
-                    inf_dict_up1['st_trainlinenm'], inf_dict_up1['st_arvlmsg2'], 
-                    inf_dict_up2['st_trainlinenm'],inf_dict_up2['st_arvlmsg2']
-            )
+                down = True
 
-                       )
-            msg_time2 = "기준 시각 : {}\n\n".format(inf_dict_down1['st_recptndt'])
-            msg_info_down = ("# {}\n{} - {}\n{} - {}\n\n".format(
 
-                    inf_dict_down1['st_updnline'], 
-                    inf_dict_down1['st_trainlinenm'], inf_dict_down1['st_arvlmsg2'], 
-                    inf_dict_down2['st_trainlinenm'],inf_dict_down2['st_arvlmsg2'])
+            if a['subwayId'] == '1001' and a['updnLine'] =="하행" and down == True:
+                inf_dict_down2["st_name"] = a['statnNm']
+                inf_dict_down2["st_updnline"] = a['updnLine']
+                inf_dict_down2["st_trainlinenm"] = a['trainLineNm'].split("-")[0]
+                inf_dict_down2["st_recptndt"] = a['recptnDt']
+                inf_dict_down2["st_arvlmsg2"] = a['arvlMsg2']
 
-                       )
+                
 
-            message = msg_intro + msg_time + msg_info_up + msg_time2 + msg_info_down
 
-            result = {"text":message}
-            end = time.time() - start
-            
-            print(end)
-            return result
-    except (requests.exceptions.ConnectionError,OSError) as e:
-        return e
+        msg_intro = "광운대역 지하철 실시간 도착정보 입니다.\n\n"
+        msg_time = "기준 시각 : {}\n\n".format(inf_dict_up1['st_recptndt'])
+        msg_info_up = ("# {}\n{} - {}\n{} - {}\n\n".format(
+
+                inf_dict_up1['st_updnline'], 
+                inf_dict_up1['st_trainlinenm'], inf_dict_up1['st_arvlmsg2'], 
+                inf_dict_up2['st_trainlinenm'],inf_dict_up2['st_arvlmsg2']
+        )
+
+                   )
+        msg_info_down = ("# {}\n{} - {}\n{} - {}\n\n".format(
+
+                inf_dict_down1['st_updnline'], 
+                inf_dict_down1['st_trainlinenm'], inf_dict_down1['st_arvlmsg2'], 
+                inf_dict_down2['st_trainlinenm'],inf_dict_down2['st_arvlmsg2'])
+
+                   )
+
+        message = msg_intro + msg_time + msg_info_up + msg_info_down
+        
+        
+        result = {"text":message}
+
+        return result
 
 if __name__ == "__main__":
     start = time.time()
     info_subway()
     end = time.time() - start
     print(end)
+
